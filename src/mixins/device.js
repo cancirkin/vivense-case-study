c620d63395b4d6486f36d6229c196bfbf4f2ece4
Hüseyin Can Çirkin <huseyin.can.cirkin@gmail.com>
2022-05-12 18:22:53 +0300

    🚀 Hot Fix.

--- src/mixins/device.js
+++ src/mixins/device.js
@@ -8,14 +8,17 @@ let device = {
     const ua = navigator.userAgent;
     if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
       this.device = "tablet";
+      return;
     } else if (
       /Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(
         ua
       )
     ) {
       this.device = "mobile";
+      return;
     }
     this.device = "desktop";
+    return;
   },
 };
 export default device;
